dfd81e31d3c1ca89c0f6c13ebb69dba6b3fe4c50
Maxime Choffat <maxime.choffat@ing.be>
2018-04-27 17:01:11 +0200

    add very simple upload service to store kml

---
+++ maps/kml_upload/app.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3003
+const upload_path = process.env.UPLOAD_PATH || 'route.kml'
+
+app.use(bodyParser.text())
+
+app.post('/route', function (req, res) {
+
+  let kml_content = req.body
+
+  // write to a new file named 2pac.txt
+
+  fs.writeFile(upload_path, kml_content, (err) => {  
+      // throws an error, you could also catch it here
+      if (err) {
+        res.status(500).send(err)
+      }
+
+      // success case, the file was saved
+      res.send('OK')
+  });
+})
+
+app.listen(port, function () {
+  console.log("kml_upload listening on port %s!", port)
+})
