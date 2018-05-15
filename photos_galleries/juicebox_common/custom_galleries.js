620cfc4f35615e70a54081d9d0aa54e2985dce42
Maxime Choffat <maxime.choffat@ing.be>
2018-05-15 15:09:02 +0200

    adding basics of manual photo galleries

---
+++ photos_galleries/juicebox_common/custom_galleries.js
@@ -0,0 +1,25 @@
+function initialize_select(galleries) {
+    var sel = document.getElementById('gallery-select');
+    for(var i = 0; i < galleries.length; i++) {
+        var opt = document.createElement('option');
+        opt.innerHTML = galleries[i].name;
+        opt.value = galleries[i].name;
+        sel.appendChild(opt);
+    }
+}
+
+function change_gallery() {
+    var gallery = document.getElementById('gallery-select').value
+    init_gallery(gallery)
+}
+
+function init_gallery(gallery_name, containerId  ='juicebox-container') {
+    new juicebox({
+        baseUrl: 'http://localhost:8080/' + gallery_name,
+        configUrl: 'http://localhost:8080/' + gallery_name + '/config.xml',
+        containerId: containerId,
+        galleryWidth: '100%',
+        galleryHeight: '100%',
+        backgroundColor: 'rgba(34,34,34,1)'
+    });
+}
