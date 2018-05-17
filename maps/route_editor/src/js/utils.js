52a4bdcd5655269bec44335382ece99ad93a0ad0
Maxime Choffat <maxime.choffat@ing.be>
2018-05-17 14:21:18 +0200

    adding auth basic header to kml saving request

--- maps/route_editor/src/js/utils.js
+++ maps/route_editor/src/js/utils.js
@@ -6,11 +6,14 @@
 export function saveRoute(kml) {
   console.log("Saving data :")
   console.log(kml)
+  var username=process.env.REACT_APP_ROUTE_KML_SAVE_USERNAME
+  var password=process.env.REACT_APP_ROUTE_KML_SAVE_PASSWORD
   return fetch(process.env.REACT_APP_ROUTE_KML_SAVE_URL, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + new Buffer(username + ":" + password).toString('base64')
     },
     body:JSON.stringify({
       kml : kml
