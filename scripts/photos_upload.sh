6a64f8634ece61434f996d598c091ff4793afebf
Maxime Choffat <maxime.choffat@ing.be>
2018-05-18 10:55:05 +0200

    prepare script update for photo script compression (local compression preferred)

--- scripts/photos_upload.sh
+++ scripts/photos_upload.sh
@@ -20,7 +20,7 @@ cd workspace
 FOUND_SOMETHING=false
 
 for archive in $(find ${PHOTOS_UPLOAD_FOLDER}/*.zip 2> /dev/null); do
-echo "Processing ${archive} archive.."
+  echo "Processing ${archive} archive.."
 
 # copy zip to WS
 cp ${archive} .
@@ -29,6 +29,11 @@ cp ${archive} .
 ARCHIVE_NAME=$(basename ${archive})
 GALLERY_NAME=$(basename ${ARCHIVE_NAME} .zip)
 unzip ${ARCHIVE_NAME}
+
+# compress photos
+# Uncomment if needed (aka local compression is not practical)
+# jpegoptim *.jpg *.jpeg
+# optipng *.png
 
 # remove zip from ws
 rm ${ARCHIVE_NAME}
@@ -45,7 +50,7 @@ rm ${archive}
 
 if [ "${FOUND_SOMETHING}" = false ]
 then
-    FOUND_SOMETHING=true
+  FOUND_SOMETHING=true
 fi
 
 done
@@ -54,8 +59,8 @@ echo "===> END OF EXTRACTION. NOW UPDATING CONFIGURATIONS <==="
 
 if [ "${FOUND_SOMETHING}" = false ]
 then
-    echo "===> No extraction has been made, no need to update configs. <==="
-   exit 0
+  echo "===> No extraction has been made, no need to update configs. <==="
+  exit 0
 fi
 
 
@@ -65,29 +70,30 @@ for GALLERY in $(find ${PHOTOS_GALLERIES_FOLDER} -type d 2> /dev/null); do
   echo ">>> Processing gallery ${GALLERY}"
 
   PHOTOS=$(find ${GALLERY} -type f -maxdepth 1 -iname "*.png" -o -iname "*.jpg" 2> /dev/null | sort)
-    
+
   if [ -z "${PHOTOS}" ]
   then
-      echo "No photo in folder. skipping it"
+    echo "No photo in folder. skipping it"
   else
     echo "PHOTOS FOUND : "
-   
-   echo '<?xml version="1.0" encoding="UTF-8"?>' > ${GALLERY}/config.xml
-   echo '<juiceboxgallery>' >> ${GALLERY}/config.xml
-   
-   for PHOTO in ${PHOTOS}; do
-       photo_path=${PHOTO#"${PHOTOS_GALLERIES_FOLDER}/"}
-       echo ${photo_path}
-       
-       echo "<image imageURL=\"${photo_path}\" thumbURL=\"${photo_path}\" linkURL=\"${photo_path}\" linkTarget=\"_blank\">" >> ${GALLERY}/config.xml
+
+    echo '<?xml version="1.0" encoding="UTF-8"?>' > ${GALLERY}/config.xml
+    echo '<juiceboxgallery>' >> ${GALLERY}/config.xml
+
+    for PHOTO in ${PHOTOS}; do
+
+      photo_path=${PHOTO#"${PHOTOS_GALLERIES_FOLDER}/"}
+      echo ${photo_path}
+
+      echo "<image imageURL=\"${photo_path}\" thumbURL=\"${photo_path}\" linkURL=\"${photo_path}\" linkTarget=\"_blank\">" >> ${GALLERY}/config.xml
        #echo "<title><![CDATA[$(basename ${photo_path})]]></title>" >> ${GALLERY}/config.xml
        echo "</image>" >> ${GALLERY}/config.xml
-   done
-   echo '</juiceboxgallery>' >> ${GALLERY}/config.xml
+     done
+     echo '</juiceboxgallery>' >> ${GALLERY}/config.xml
 
-   echo "{ \"name\": \"$(basename ${GALLERY})\"}," >> ${PHOTOS_GALLERIES_FOLDER}/common/galleries.json
-  fi
-done
+     echo "{ \"name\": \"$(basename ${GALLERY})\"}," >> ${PHOTOS_GALLERIES_FOLDER}/common/galleries.json
+   fi
+ done
 # remove tailing comma
 sed -i '$ s/.$//'  ${PHOTOS_GALLERIES_FOLDER}/common/galleries.json
 echo "]" >> ${PHOTOS_GALLERIES_FOLDER}/common/galleries.json
